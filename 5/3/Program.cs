ed9d714d6ed28f4f891cf4086f2ec4e99063de52
Artur <fox9910@gmail.com>
2022-12-20 00:29:49 +0500

    Дз 5

---
+++ 5/3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+// минимальным элементом массива.
+
+void FillArrayRandomNumbers(double [] Random) // Генерация случайных чисел
+{
+    for(int i = 0; i < Random.Length; i++)
+    {
+        Random[i] = new Random().Next(-99.0, 99.0);
+    }
+}
+void PrintArray(double[] Random) // Заполнение массива 
+{
+    Console.Write("[ ");
+    for(int i = 0; i < Random.Length; i++)
+    {
+        Console.Write(Random[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double [] Random = new double [size];
+FillArrayRandomNumbers(Random);
+Console.WriteLine();
+Console.WriteLine("Полученый одномерный массив случайных чисел: ");
+PrintArray(Random);
