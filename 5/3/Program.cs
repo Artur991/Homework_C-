182349e0fc84275d11745506797c212ea17a30c2
Artur <fox9910@gmail.com>
2022-12-20 22:11:23 +0500

    Дз 5

--- 5/3/Program.cs
+++ 5/3/Program.cs
@@ -1,22 +1,73 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и 
 // минимальным элементом массива.
 
-void FillArrayRandomNumbers(double [] Random) // Генерация случайных чисел
+void FillArrayRandomNumbers(double [] Random) //Генерация случайных вещественых чисел
 {
+    Random random = new Random();
     for(int i = 0; i < Random.Length; i++)
     {
-        Random[i] = new Random().Next(-99.0, 99.0);
+        Random[i] = random.NextDouble() * 1000;
     }
 }
-void PrintArray(double[] Random) // Заполнение массива 
+void PrintDoubleArray(double[] Random) //Заполнение массива 
 {
     Console.Write("[ ");
     for(int i = 0; i < Random.Length; i++)
     {
-        Console.Write(Random[i] + " ");
+        Console.Write(String.Format("{0:00.00}  ", Random[i]));
     }
     Console.Write("]");
     Console.WriteLine();
+}
+// void FindMinAndMax(double[] Random) //Поиск минимального и максимального элиммента
+// {
+// double min = Random[0];
+// double max = Random[0];
+ 
+// double minIndex = 0;
+// double maxIndex = 0;
+ 
+// for (int i = 0; i < Random.Length; i++)
+// {
+//     if (Random[i] < min)
+//     {
+//         min = Random[i];
+//         minIndex = i;
+//     }
+//     else if (Random[i] > max)
+//     {
+//         max = Random[i];
+//         maxIndex = i;
+//     }
+// }
+//  Console.WriteLine(String.Format("Минимальный элимент массива: [ {0:00.00} ]",min));
+//  Console.WriteLine(String.Format("Максимальный элимент массива: [ {0:00.00} ]",max));
+// }
+void FindDifference(double[] Random) //Разница между минимальным и максимальным элеменнтом массива
+{
+double FindMinNumber = Random[0];
+double FindMaxNumber = Random[0];
+ 
+double minIndex = 0;
+double maxIndex = 0;
+ 
+for (int i = 0; i < Random.Length; i++)
+{
+    if (Random[i] < FindMinNumber)
+    {
+        FindMinNumber = Random[i];
+        minIndex = i;
+    }
+    else if (Random[i] > FindMaxNumber)
+    {
+        FindMaxNumber = Random[i];
+        maxIndex = i;
+    }
+}
+Console.WriteLine(String.Format("Минимальный элимент массива: [ {0:00.00} ]",FindMinNumber));
+Console.WriteLine(String.Format("Максимальный элимент массива: [ {0:00.00} ]",FindMaxNumber));
+Console.WriteLine(String.Format("Разница между найдеными элиментами массива: [ {0:00.00} ]",FindMaxNumber - FindMinNumber));
+ 
 }
 Console.WriteLine("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -24,4 +75,6 @@ double [] Random = new double [size];
 FillArrayRandomNumbers(Random);
 Console.WriteLine();
 Console.WriteLine("Полученый одномерный массив случайных чисел: ");
-PrintArray(Random);
+PrintDoubleArray(Random);
+// FindMinAndMax(Random);
+FindDifference(Random);
