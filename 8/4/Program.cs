4d5b6aba146a14da92c640ec20893dc97a4d53ac
Artur <fox9910@gmail.com>
2023-01-14 01:29:11 +0500

    Дз8 задача 4

--- 8/4/Program.cs
+++ 8/4/Program.cs
@@ -7,8 +7,6 @@
 // 11 22 78
 // 12 47 96
 // 25 87 88
-
-
 // класс с методами расширения
 
 
@@ -18,117 +16,74 @@ int rowcount = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Количество столбцов массива: ");
 int columcount = Convert.ToInt32(Console.ReadLine());
 
-int [,] arr = new int [rowcount, columcount] ;
-int N = rowcount * columcount;
-int [] arr2 = new int [N] ;
-int examinationNumber = 0;
+int [,] arr = new int [rowcount, columcount];
+int[] arr2 = new int[rowcount*columcount];
+Console.WriteLine();
+Console.WriteLine("Вывод массива по заданым параметрам: ");
+FillRandom(arr, new Random()); // Генерация массива
+WriteArray(arr);               // Печать 2х мерного масива
+Console.WriteLine();
+Console.WriteLine("Вывод однострочного массива: ");
+ToJagged(arr);                    // Вывод в однострочный массив
 
-void fillArray(int[,] arr)          
+void FillRandom(int[,] array, Random random)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    int element = random.Next(10, 100);
+ 
+                    while (Contains(array, element))
+                    {
+                        element = random.Next(10, 100);
+                    }
+ 
+                    array[i, j] = element;
+ 
+                }
+            }
+ 
+        }
+
+bool Contains(int[,] array, int element)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == element)
+                        return true;
+                }
+            }
+ 
+            return false;
+        }
+
+void WriteArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+ 
+                Console.WriteLine();
+            }
+        }
+
+static T[][] ToJagged<T>(T[,] array) 
 {
-    for(int i = 0; i < arr.GetLength(0); i++)
+    var result = new T[array.GetLength(0)][];
+    for (int i = 0; i < result.Length; i++) 
     {
-        for(int j = 0; j < arr.GetLength(1); j++)
+        result[i] = new T[array.GetLength(1)];
+        for (int j = 0; j < result[i].Length; j++) 
         {
-        arr[i,j] = new Random().Next(10,100); 
-        }
+            result[i][j] = array[i, j];
+            Console.Write(result[i][j] + " ");
+        }   
     }
+    return result;
 }
-
-void printArray(int[,] array)
-{
-    
-    for(int i = 0; i < arr.GetLength(0); i++)
-    {
-        Console.Write("{ ");
-        
-        for(int j = 0; j < arr.GetLength(1); j++)
-        {
-        Console.Write(arr[i,j] + " "); 
-        }
-        Console.Write("}");
-        Console.WriteLine();
-    }
-    
-    Console.WriteLine();
-}
-
-void sortArray(int[,] array)
-{
-    Console.Write("{ ");
-    for(int i = 0; i < arr.GetLength(0); i++)
-    { 
-        for(int j = 0; j < arr.GetLength(1); j++)
-        {
-           for (int s = j; s < arr.GetLength(1) - 1; s++)
-           {
-           if (arr[i, j] < arr[j, s])
-           {
-           int temp = arr[j, s];
-           if(arr[i, j] == arr[j, s])
-           {
-            arr[i, j] = 0;
-           }
-           } 
-           }
-        Console.Write(arr[i,j] + " ");
-        }
-    }  
-    Console.Write("}");
-    Console.WriteLine();
-}
-
-// bool Contains(int [,] array, int element)
-// {
-//     for(int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for(int j = 0; j < arr.GetLength(1); j++)
-//         {
-//             if (arr[i,j] == element)
-//             {
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
-// }
-
-// bool ExaminationArray(int[,] array)          
-// {
-    
-//     for(int i = 0; i<array.GetLength(0);i++)
-// {
-    
-//       if(array[i,0]!=0)
-//   {
-//       for(int j = i+1;j< array.GetLength(1); j++)
-//       {
-//          if(array[i,j]==array[i,j])
-//          {
-//          array[i,j]== 0;
-//          }
-//       }
-//    }
-//     }
-// }
-// for(int i = 0;i<years.Lenght;i++)
-// {
-//    if(years[i]!="0")
-//    {
-//       Console.WriteLine(years[i]); или newArrayYears.Add(years[i])
-//    }
-// }
-// }
-
-fillArray(arr);
-printArray(arr);
-sortArray(arr);
-// Contains(arr, examinationNumber);
-ExaminationArray(examinationNumber);
-
-
-// Console.Write(string.Join(' ', Array)); // сформирует строку из элементов и разделит их 'значением'
-// string str = Convert.ToString(arr2);
-// str = string.Join(' ', arr.Cast<int>());
-
-// оператор X ? Y : Z  (N>0)?1:-1
-
