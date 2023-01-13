bd209bafb735e92061ca2b73b619f63afa89ed3c
Artur <fox9910@gmail.com>
2023-01-14 01:39:26 +0500

    Дз8

--- 8/4/Program.cs
+++ 8/4/Program.cs
@@ -9,81 +9,76 @@
 // 25 87 88
 // класс с методами расширения
 
-
-
-Console.WriteLine("Создание двумерного массива.   Количество строк массива: ");
-int rowcount = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Количество столбцов массива: ");
-int columcount = Convert.ToInt32(Console.ReadLine());
-
-int [,] arr = new int [rowcount, columcount];
-int[] arr2 = new int[rowcount*columcount];
-Console.WriteLine();
-Console.WriteLine("Вывод массива по заданым параметрам: ");
-FillRandom(arr, new Random()); // Генерация массива
-WriteArray(arr);               // Печать 2х мерного масива
-Console.WriteLine();
-Console.WriteLine("Вывод однострочного массива: ");
-ToJagged(arr);                    // Вывод в однострочный массив
-
-void FillRandom(int[,] array, Random random)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    int element = random.Next(10, 100);
- 
-                    while (Contains(array, element))
-                    {
-                        element = random.Next(10, 100);
-                    }
- 
-                    array[i, j] = element;
- 
-                }
-            }
- 
-        }
-
-bool Contains(int[,] array, int element)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    if (array[i, j] == element)
-                        return true;
-                }
-            }
- 
-            return false;
-        }
-
-void WriteArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
- 
-                Console.WriteLine();
-            }
-        }
-
-static T[][] ToJagged<T>(T[,] array) 
+internal class Program
 {
-    var result = new T[array.GetLength(0)][];
-    for (int i = 0; i < result.Length; i++) 
+    private static void Main()
     {
-        result[i] = new T[array.GetLength(1)];
-        for (int j = 0; j < result[i].Length; j++) 
-        {
-            result[i][j] = array[i, j];
-            Console.Write(result[i][j] + " ");
-        }   
+        Console.WriteLine("Создание двумерного массива.   Количество строк массива: ");
+        int rowcount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Количество столбцов массива: ");
+        int columcount = Convert.ToInt32(Console.ReadLine());
+        int[,] arr = new int[rowcount, columcount];
+        int[] arr2 = new int[rowcount * columcount];
+        Console.WriteLine();
+        Console.WriteLine("Вывод массива по заданым параметрам: ");
+        FillRandom(arr, new Random()); // Генерация массива
+        WriteArray(arr);               // Печать 2х мерного масива
+        Console.WriteLine();
+        Console.WriteLine("Вывод однострочного массива: ");
+        ToJagged(arr);                    // Вывод в однострочный массив
     }
-    return result;
+    static void FillRandom(int[,] array, Random random)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                int element = random.Next(10, 100);
+                while (Contains(array, element))
+                {
+                    element = random.Next(10, 100);
+                }
+                array[i, j] = element;
+            }
+        }
+    }
+    static bool Contains(int[,] array, int element)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] == element)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+    static void WriteArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+    static T[][] ToJagged<T>(T[,] array)
+    {
+        var result = new T[array.GetLength(0)][];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = new T[array.GetLength(1)];
+            for (int j = 0; j < result[i].Length; j++)
+            {
+                result[i][j] = array[i, j];
+                Console.Write(result[i][j] + " ");
+            }
+        }
+        return result;
+    }
 }
