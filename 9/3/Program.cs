9c8d8223e233f3c157daecb02fbe60cd616b67bf
Artur <fox9910@gmail.com>
2023-01-15 18:24:17 +0500

    Дз9 зд 3

--- 9/3/Program.cs
+++ 9/3/Program.cs
@@ -3,6 +3,7 @@
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9 (?)
 // m = 3, n = 2 -> A(m,n) = 29 (?)
+
 Console.WriteLine("Вычисления функции Аккермана: ");
 Console.WriteLine("Введите натуральное число M: ");
 int M = Convert.ToInt32(Console.ReadLine());
@@ -13,11 +14,8 @@ int Akerman(int M, int N)
 {
     if (M == 0)
         return N + 1;
-    else
-      if ((M != 0) && (N == 0))
-        return Akerman(M - 1, 1);
-    else
-        return Akerman(M - 1, Akerman(M, N - 1));
+    else if ((M != 0) && (N == 0)) return Akerman(M - 1, 1);
+    else return Akerman(M - 1, Akerman(M, N - 1));
 }
 
 Console.WriteLine("Результат: " + Akerman(M, N)); 
