290ccca857220f77ede691c64dcfac979910bba3
Artur <fox9910@gmail.com>
2023-01-15 17:41:03 +0500

    Дз9 зд 1

---
+++ 9/1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: 
+// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+Console.WriteLine("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string Numbers (int N, int B = 1)
+{
+    if (N >= 1) return $" {N} " + Numbers(N - 1);
+    else return String.Empty;
+
+}
+
+Console.WriteLine("Полученые натуральные числа от N до  1");
+Console.WriteLine(Numbers(N) + " ");
+
+
